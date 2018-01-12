293f8ccee9a4feb976c32602bd7e7ec9c35db17c
Nicolas Tarzia <github@nicolastarzia.com>
2018-01-12 16:12:33 -0200

    :books: Product documentation

--- VTexConnector/CatalogSystem/Product.cs
+++ VTexConnector/CatalogSystem/Product.cs
@@ -6,6 +6,9 @@ using VTexConnector.Entity;
 
 namespace VTexConnector.CatalogSystem
 {
+    /// <summary>
+    /// Get Product info
+    /// </summary>
     public class Product : BaseClass
     {
         public Product() : base()
@@ -13,6 +16,11 @@ namespace VTexConnector.CatalogSystem
 
         }
 
+        /// <summary>
+        /// Get product By Internal ID
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns>Return request from VTex</returns>
         public async Task<ProductModel> GetByIdAsync(int id)
         {
             string productUrl = Util.FormatUrl(Consts.PRODUCT_GETBYID);
@@ -20,6 +28,11 @@ namespace VTexConnector.CatalogSystem
             return await ExtractObject<ProductModel>(productUrl);
         }
 
+        /// <summary>
+        /// Get product By reference id
+        /// </summary>
+        /// <param name="refId">Reference ID</param>
+        /// <returns>Return request from VTex</returns>
         public async Task<ProductModel> GetByRefIdAsync(string refId)
         {
             string productUrl = Util.FormatUrl(Consts.PRODUCT_GETBYREFID);
