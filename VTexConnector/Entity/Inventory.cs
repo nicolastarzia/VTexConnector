85053e14ae6dc39b576f8ec99cd7db20ffcb13fd
Nicolas Tarzia <github@nicolastarzia.com>
2018-04-04 14:39:28 -0300

    Add Inventory

---
+++ VTexConnector/Entity/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace VTexConnector.Entity
+{
+    public class Balance
+    {
+        public string warehouseId { get; set; }
+        public string warehouseName { get; set; }
+        public int totalQuantity { get; set; }
+        public int reservedQuantity { get; set; }
+        public bool hasUnlimitedQuantity { get; set; }
+    }
+
+    public class InventoryModel
+    {
+        public string skuId { get; set; }
+        public List<Balance> balance { get; set; }
+    }
+
+    public class InventoryUpdateModel
+    {
+        public bool unlimitedQuantity { get; set; }
+        public System.DateTime dateUtcOnBalanceSystem { get; set; }
+        public int quantity { get; set; }
+    
+    }
+}
--- VTexConnector/Logistics/Inventory.cs
+++ VTexConnector/Logistics/Inventory.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using VTexConnector.Entity;
 
 namespace VTexConnector.Logistics
 {
@@ -11,6 +14,27 @@ namespace VTexConnector.Logistics
         public Inventory() : base()
         {
             throw new NotImplementedException("To be develop!");
+        }
+
+        public async Task<InventoryModel> GetPriceAsync(int id)
+        {
+            string productUrl = Util.FormatUrl(Consts.INVENTORY_GETBYSKU);
+            productUrl = productUrl.Replace("{{skuId}}", id.ToString());
+            return await GetObjectFromUri<InventoryModel>(productUrl);
+        }
+
+
+        public async Task<bool> UpdateInventory(int itemId, int warehouseId, InventoryUpdateModel price)
+        {
+            string productUrl = Util.FormatUrl(Consts.INVENTORY_UPDATEINVENTORYBYSKUWAREHOUSE);
+            productUrl = productUrl.Replace("{{skuId}}", itemId.ToString());
+            productUrl = productUrl.Replace("{{warehouseId}}", warehouseId.ToString());
+
+            var priceModel = JsonConvert.SerializeObject(price);
+            var httpResponse = await PutToUri(productUrl, new StringContent(priceModel));
+
+            return httpResponse.IsSuccessStatusCode;
+
         }
     }
 }
